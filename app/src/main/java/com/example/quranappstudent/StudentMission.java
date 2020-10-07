977367beb0323e1f00cbe828a1febe371aae42a4
mohammed-A-yousif <71412422+mohammed-A-yousif@users.noreply.github.com>
2020-10-07 10:07:28 +0200

    recycler inside a fragment

--- app/src/main/java/com/example/quranappstudent/StudentMission.java
+++ app/src/main/java/com/example/quranappstudent/StudentMission.java
@@ -8,11 +8,36 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentMission extends Fragment {
+    public StudentMission(){
+
+    }
+    private MissionsAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mission_calender,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_mission_calender, container, false);
+        RecyclerView recyclerView = rootView.findViewById(R.id.missions_recyclerView);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        List<Contact> listItems = new ArrayList<>();
+
+        for (int i = 0; i <10; i++) {
+            Contact listItem = new Contact("Mohammed Ahmed" + (i + 1), "0909041441", "9/26/2020");
+            listItems.add(listItem);
+        }
+        adapter = new MissionsAdapter(listItems, getActivity());
+        recyclerView.setAdapter(adapter);
+
+
+        return rootView;
     }
+
 }
