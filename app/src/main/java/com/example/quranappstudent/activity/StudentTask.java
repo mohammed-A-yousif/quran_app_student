0f9fb7c6b47e6479ff81f6efee30171cf6f40e98
mohamed-A-ahmed <moh999ah@gmail.com>
2021-01-31 22:46:33 +0200

    student task dialog view

--- app/src/main/java/com/example/quranappstudent/activity/StudentTask.java
+++ app/src/main/java/com/example/quranappstudent/activity/StudentTask.java
@@ -7,10 +7,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -66,9 +69,41 @@ public class StudentTask extends Fragment {
         adapter.setOnItemClickListener(new TaskAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
+                Task mission = listItems.get(position);
+                String title = mission.getTaskName();
 
-                Toast.makeText(getActivity(),"View Yr Dialog Now ^_*",Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getActivity(),"View Yr Dialog Now ^_*",Toast.LENGTH_SHORT).show();
                 //              ################
+
+                final AlertDialog dialogBuilder = new AlertDialog.Builder(getActivity()).create();
+                LayoutInflater inflater1 = getActivity().getLayoutInflater();
+
+                View dialogView = inflater1.inflate(R.layout.task_dialog, null);
+                TextView titleTaskTextView= dialogView.findViewById(R.id.task_dialog_tv);
+                titleTaskTextView.setText(title);
+
+                dialogBuilder.setView(dialogView);
+
+                FrameLayout mDialogNo = dialogView.findViewById(R.id.frmNo);
+                mDialogNo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getActivity(),"انجزي الشغله دي سرعه ^_^" ,Toast.LENGTH_LONG).show();
+                        dialogBuilder.dismiss();
+                    }
+                });
+
+                FrameLayout mDialogOk = dialogView.findViewById(R.id.frmOk);
+                mDialogOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getActivity(),"فااااالحه ^_^" ,Toast.LENGTH_LONG).show();
+                        dialogBuilder.cancel();
+                    }
+                });
+
+
+                dialogBuilder.show();
             }
         });
 
